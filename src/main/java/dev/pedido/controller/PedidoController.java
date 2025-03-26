4cedc6d0a5850c94f5163a2568c82bd390aa53c2
MM <mouramathdev@gmail.com>
2025-03-26 08:01:56 -0300

    rabbitmq

--- src/main/java/dev/pedido/controller/PedidoController.java
+++ src/main/java/dev/pedido/controller/PedidoController.java
@@ -3,6 +3,8 @@ package dev.pedido.controller;
 
 import dev.pedido.model.Pedido;
 import dev.pedido.service.PedidoService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,16 +13,22 @@ import java.util.List;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
+    private final RabbitTemplate rabbitTemplate;
     private final PedidoService pedidoService;
 
 
-    public PedidoController(PedidoService pedidoService) {
+    public PedidoController(RabbitTemplate rabbitTemplate, PedidoService pedidoService) {
+        this.rabbitTemplate = rabbitTemplate;
         this.pedidoService = pedidoService;
     }
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
+
 
     @PostMapping
     public String criarPedido(@RequestBody Pedido pedido) {
-        pedidoService.salvarPedido(pedido);
+        Pedido pedidoSalvo = pedidoService.salvarPedido(pedido);
+        rabbitTemplate.convertAndSend(exchange: "", routingKey, pedidoSalvo.getDescricao());
         return "Pedido salvo e enviado para processamento: " +pedido.getDescricao();
     }
 
