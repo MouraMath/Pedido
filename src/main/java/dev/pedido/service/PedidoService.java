84f2f9b1be084b004ae02e6edea442c12112c10e
MM <mouramathdev@gmail.com>
2025-03-26 07:35:18 -0300

    Service

--- src/main/java/dev/pedido/service/PedidoService.java
+++ src/main/java/dev/pedido/service/PedidoService.java
@@ -1,8 +1,12 @@
 package dev.pedido.service;
 
 
+import dev.pedido.model.ItemPedido;
+import dev.pedido.model.Pedido;
 import dev.pedido.repository.PedidoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PedidoService {
@@ -12,4 +16,21 @@ public class PedidoService {
     public PedidoService(PedidoRepository pedidoRepository) {
         this.pedidoRepository = pedidoRepository;
     }
+
+    public Pedido salvarPedido(Pedido pedido) {
+
+        if(pedido.getItens() != null){
+            for(ItemPedido item : pedido.getItens()){
+                item.setPedido(pedido);
+            }
+        }
+        return pedidoRepository.save(pedido);
+
+    }
+
+    public List<Pedido> listarPedidos(){
+        return pedidoRepository.findAll();
+    }
+
+
 }
